8f4d7febbe2059c0c7a7ece62a2dcf0e2a9970c8
Jalilov <javohirjaliloff@gmail.com>
2023-02-11 10:30:49 +0500

    sdf

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-   name='numpyTest',
+   name='basicNumpyTest',
    version='0.1.0',
    author='Javohir Jalilov',
    author_email='javohirjaliloff@gmail.com',
