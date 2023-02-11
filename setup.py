7618247c99dc18931376d83ca61807e3f3afa4a1
Jalilov <javohirjaliloff@gmail.com>
2023-02-11 10:31:56 +0500

    .

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ setup(
    author='Javohir Jalilov',
    author_email='javohirjaliloff@gmail.com',
    packages=['basicNumpyTest'],
-   scripts=['bin/script1','bin/script2'],
-   url='http://pypi.python.org/pypi/numpytest/',
    license='LICENSE.txt',
    description='This is numpy basic test',
-   install_requires=[
-       "pytest",
-       "numpy"
-   ],
 )
